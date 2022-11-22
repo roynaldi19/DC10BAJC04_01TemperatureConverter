80dffd0daa194dbfe1e6d6bc09642e222909a2b1
roynaldi19 <roynaldi19@gmail.com>
2022-11-22 11:10:32 +0700

    statefull converter

--- app/src/main/java/com/roynaldi19/dc10bajc04_01temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc10bajc04_01temperatureconverter/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,8 +11,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +26,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DC10BAJC04_01TemperatureConverterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    StatefulTemperatureInput()
+
+                }
 
 
             }
@@ -36,8 +44,8 @@ class MainActivity : ComponentActivity() {
 fun StatefulTemperatureInput(
     modifier: Modifier = Modifier,
 ) {
-    var input = ""
-    var output = ""
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
     Column(modifier.padding(16.dp)) {
         Text(
             text = stringResource(R.string.stateful_converter),
@@ -47,12 +55,20 @@ fun StatefulTemperatureInput(
             value = input,
             label = { Text(stringResource(R.string.enter_celsius))},
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            onValueChange = {}
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
         )
         Text(stringResource(R.string.temperature_fahrenheit, output))
 
     }
 }
+
+private fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
 
 
 @Preview(showBackground = true)
